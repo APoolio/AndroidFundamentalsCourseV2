18bb69be9292d5a3562e582c4e62859049c907cc
APoolio <andrewspooleap@gmail.com>
2020-02-03 09:43:27 -0500

    Finished 9.1 Shared Preferences

---
+++ HelloSharedPrefs/app/src/main/java/com/example/android/hellosharedprefs/MainActivity.java
@@ -0,0 +1,155 @@
+/*
+ * Copyright (C) 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.hellosharedprefs;
+
+import android.content.SharedPreferences;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * HelloSharedPrefs is an adaptation of the HelloToast app from chapter 1.
+ * It includes:
+ * - Buttons for changing the background color.
+ * - Maintenance of instance state.
+ * - Themes and styles.
+ * - Read and write shared preferences for the current count and the color.
+ * <p>
+ * This is the starter code for HelloSharedPrefs.
+ */
+public class MainActivity extends AppCompatActivity {
+    // Current count
+    private int mCount = 0;
+    // Current background color
+    private int mColor;
+    // Text view to display both count and color
+    private TextView mShowCountTextView;
+
+    // Key for current count
+    private final String COUNT_KEY = "count";
+    // Key for current color
+    private final String COLOR_KEY = "color";
+
+    //Shared reference object
+    private SharedPreferences mPreferences;
+    //shared preference file
+    private String sharedPrefFile = "com.example.android.hellosharedprefs";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize views, color
+        mShowCountTextView = findViewById(R.id.count_textview);
+        mColor = ContextCompat.getColor(this, R.color.default_background);
+
+        //Opens file at sharedPrefFile with MODE_PRIVATE
+        mPreferences = getSharedPreferences(sharedPrefFile, MODE_PRIVATE);
+
+        //getting int with the COUNT_KEY. If the key cannot be found then 0 is the default value
+        mCount = mPreferences.getInt(COUNT_KEY, 0);
+
+        //Setting the TextView to COUNT_KEY value
+        mShowCountTextView.setText(String.format("%s", mCount));
+
+        //getting the current color
+        mColor = mPreferences.getInt(COLOR_KEY, mColor);
+
+        //Setting the correct background color
+        mShowCountTextView.setBackgroundColor(mColor);
+    }
+
+
+    /**
+     * OnPause is similar to onSaveInstance but it is used to save data
+     * across app uses
+     */
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+
+        /*//Needed to write to a shared preference object
+        SharedPreferences.Editor preferencesEditor = mPreferences.edit();
+
+        //Used to store the current color and current count
+        preferencesEditor.putInt(COUNT_KEY, mCount);
+        preferencesEditor.putInt(COLOR_KEY, mColor);
+
+        //Saving the preferences asynchronously off of the UI thread
+        preferencesEditor.apply();*/
+
+        SharedPreferences.Editor preferencesEditor = mPreferences.edit();
+        preferencesEditor.putInt(COUNT_KEY, mCount);
+        preferencesEditor.putInt(COLOR_KEY, mColor);
+        preferencesEditor.apply();
+    }
+
+    /**
+     * Handles the onClick for the background color buttons. Gets background
+     * color of the button that was clicked, and sets the TextView background
+     * to that color.
+     *
+     * @param view The view (Button) that was clicked.
+     */
+    public void changeBackground(View view) {
+        int color = ((ColorDrawable) view.getBackground()).getColor();
+        mShowCountTextView.setBackgroundColor(color);
+        mColor = color;
+    }
+
+    /**
+     * Handles the onClick for the Count button. Increments the value of the
+     * mCount global and updates the TextView.
+     *
+     * @param view The view (Button) that was clicked.
+     */
+    public void countUp(View view) {
+        mCount++;
+        mShowCountTextView.setText(String.format("%s", mCount));
+    }
+
+    /**
+     * Handles the onClick for the Reset button. Resets the global count and
+     * background variables to the defaults and resets the views to those
+     * default values.
+     *
+     * @param view The view (Button) that was clicked.
+     */
+    public void reset(View view)
+    {
+        // Reset count
+        mCount = 0;
+        mShowCountTextView.setText(String.format("%s", mCount));
+
+        // Reset color
+        mColor = ContextCompat.getColor(this,
+                R.color.default_background);
+        mShowCountTextView.setBackgroundColor(mColor);
+
+        //Editor to clear saved data
+        SharedPreferences.Editor preferenceEditor = mPreferences.edit();
+        
+        preferenceEditor.clear();
+
+        preferenceEditor.apply();
+
+    }
+}
